f16bb2cda9a4fe5328f38f2cd492e39c6ba6ab5c
Mark Gomes <markgomes96@gmail.com>
2020-10-03 03:40:31 -0400

    finished prototyping levels

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [Header("References")]
+    public Transform playerBody;
+    [Header("Settings")]
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        // Hide and lock cursor to center of screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotate player up-down with vertical mouse movement
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Rotate player left-right with horizontal mouse movement
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    // private reference
+    CharacterController characterController;
+
+    [Header("References")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    [Header("Settings")]
+    public float speed = 12f;
+    public float accelScalar = 3.0f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3.0f;
+
+    Vector3 velocity;
+    float currentSpeed;
+    bool isGrounded;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // check if character is grounded at start of frame
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // Zero out vertical velocity when grounded
+        if (isGrounded && velocity.y < 0f)
+        {
+            velocity.y = -2f;
+        }
+
+        // Get horizontal movement input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Zero out current speed when no input
+        if (x == 0f && z == 0f)
+        {
+            currentSpeed = 0f;
+        }
+
+        // Add x & z component of move vector
+        Vector3 moveVect = transform.right * x + transform.forward * z;
+
+        // Normalize horizontal move vector components
+        moveVect = moveVect.normalized;
+
+        // Lerp current speed to max speed
+        currentSpeed = Mathf.Lerp(currentSpeed, speed, Time.deltaTime * accelScalar);
+        moveVect *= currentSpeed;
+
+        // Apply jump velocity if player grounded
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Apply gravity to velocity
+        velocity.y += gravity * Time.deltaTime;
+
+        // Apply velocity to move vector
+        moveVect.y = velocity.y;
+
+        // Move player by move vector
+        characterController.Move(moveVect * Time.deltaTime);
+    }
+}
